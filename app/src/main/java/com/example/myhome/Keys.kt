6d8a067c902b88c6f68c5de6bed0bf41076c4931
Aditya-Natarajan <aditya.natarajan1@gmail.com>
2025-09-08 22:21:25 +0530

    Updated Keys

--- app/src/main/java/com/example/myhome/Keys.kt
+++ app/src/main/java/com/example/myhome/Keys.kt
@@ -2,7 +2,7 @@ package com.example.myhome
 
 class Keys {
     companion object {
-        const val SSID = "Apoorva5g"       // ✅ const for compile-time constants
-        const val Password = "apoorva2003sp@"
+        const val SSID = "ESP32_Config"       // ✅ const for compile-time constants
+        const val Password = "12345678"
     }
 }
